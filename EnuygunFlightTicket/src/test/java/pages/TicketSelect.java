181a0e5b2265d5218c933d0bbb60536cb8688271
AliOzkn <alizkann@gmail.com>
2022-08-21 17:51:27 +0300

    .

--- EnuygunFlightTicket/src/test/java/pages/TicketSelect.java
+++ EnuygunFlightTicket/src/test/java/pages/TicketSelect.java
@@ -17,14 +17,18 @@ public class TicketSelect extends BasePage {
     }
 
     public void selectFlights() {
+
         List<WebElement> flightNotFound = driver.findElements(By.xpath("//p[@class='result-empty-text']"));
+        //List of departure flights.
         List<WebElement> departureFlights = driver.findElements(By.xpath("//div[@class='roundTrip departure']//div[@data-booking-provider]"));
+        //List of return flights.
         List<WebElement> returnFlights = driver.findElements(By.xpath("//div[@class='roundTrip return']//div[@data-booking-provider]"));
+
 
         if (flightNotFound.isEmpty()) {
 
             for (int i = 0; i < 10; i++) {
-
+                //If data-booking-provider value equals properties-provider value. Or contains "sabre,amadeus, galileo".
                 if (departureFlights.get(i).getAttribute("data-booking-provider").contains(DriverSetup.properties.getProperty("provider").toLowerCase())
                         || departureFlights.get(i).getAttribute("data-booking-provider").contains("sabre")
                         || departureFlights.get(i).getAttribute("data-booking-provider").contains("amadeus")
@@ -38,12 +42,13 @@ public class TicketSelect extends BasePage {
                         throw new RuntimeException(e);
                     }
                     break;
+                    //If there are no tickets with the desired provider, enter here.
                 } else {
                     System.out.println("Provider ile uyumlu ucus bulunamadi.");
                     driver.close();
                 }
             }
-
+            // If there are no tickets as requested, enter here.
         } else {
             System.out.println("Belirtilen aktarma ve tarih kriterlerine uygun ucus bulunamadi.");
             driver.close();
